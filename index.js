6a6cb6825bf390510fcf769208d31d6f3ed9fab0
Grace Han <ohlookitsgrace@gmail.com>
2018-11-02 00:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
   terminatorButton.addEventListener('click', fillInMovieData)
   
 });
+
 function fillInMovieData() {
   const currentMovie = movies['Titanic']
   
